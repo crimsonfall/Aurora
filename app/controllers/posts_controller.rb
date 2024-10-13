b26509fb538ff61534d9ef43f280010fb33d934c
crimsonfall <150260152+crimsonfall@users.noreply.github.com>
2024-10-13 15:18:10 -0500

    Added edit and update functionality to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,10 +14,26 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to root_path
+      redirect_to @post
     else
-      render "new"
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
   end
 
   private
