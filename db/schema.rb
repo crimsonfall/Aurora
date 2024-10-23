e235f475a2f96f353f57b45f3d560376e293b797
crimsonfall <150260152+crimsonfall@users.noreply.github.com>
2024-10-23 13:25:43 -0500

    Associated posts with user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_19_234311) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_23_171423) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_19_234311) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
