773b2a60620d2fafc9beb2814eab58b9d8fa94b5
crimsonfall <150260152+crimsonfall@users.noreply.github.com>
2024-10-24 14:49:37 -0500

    Added basic comments form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+  end
+
+  if @comment.save
+    redirect_to @post
+  else
+    render 'new'
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
